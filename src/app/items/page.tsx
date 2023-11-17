f95f8b1339cf7a1285ab920c901de8dba03cd56f
mariel torres <marieltorres97@gmail.com>
2023-11-17 15:32:53 -0300

    fix:mt/sonarcloudbug div-non-interactive

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -2,7 +2,6 @@
 import Layout from "@/layout/Layout";
 import {IItemApiResponse} from "@/features/items/models";
 import {getData} from "@/features/shared/actions/getData";
-import {List} from "@/features/items/organisms/List";
 import {ItemsTemplate} from "@/features/items/template/ItemsTemplate";
 
 export default async function Page()
