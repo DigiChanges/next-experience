735482a8d7ee8c9fbfcd95c2d3f4267cc1216a65
Mariel Torres <marieltorres97@gmail.com>
2023-12-08 17:08:49 -0300

    feat:mt/loadings/and/alerts

--- src/features/items/organisms/formUpdate/FormUpdate.tsx
+++ src/features/items/organisms/formUpdate/FormUpdate.tsx
@@ -9,14 +9,14 @@ import { modalSchema } from '@/features/items/validations/modalSchema';
 import Link from 'next/link';
 import { updateItem } from '@/features/items/actions/ItemAction';
 import { toast } from 'react-toastify';
-import {useRouter} from "next/navigation";
+import { useRouter } from 'next/navigation';
 
 interface IProps{
     id: string;
     data:{ name: string, type: number}
 }
 export const FormUpdate: React.FC<IProps> = ({ id, data }) => {
-  const router = useRouter()
+  const router = useRouter();
   const { register, handleSubmit, formState: { errors } } = useForm<Item>({
     defaultValues:{
       name : data.name,
@@ -31,7 +31,7 @@ export const FormUpdate: React.FC<IProps> = ({ id, data }) => {
       success: 'The item was updated correctly',
       pending:'Updating item...'
     });
-    router.push('/items')
+    router.push('/items');
   };
   return (
     <form  onSubmit={handleSubmit(async(data) => { await updateAction(data); })}>
