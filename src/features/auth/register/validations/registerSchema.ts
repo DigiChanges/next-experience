a928c838209fbf02e0cc011655f4796f127417b1
Mariel Torres <marieltorres97@gmail.com>
2023-12-08 17:48:24 -0300

    feat:mt/loadings/and/alerts

--- src/features/auth/register/validations/registerSchema.ts
+++ src/features/auth/register/validations/registerSchema.ts
@@ -4,7 +4,7 @@ import * as yup from 'yup';
 export const registerSchema = yup
     .object()
     .shape({
-      username: yup.string().matches(/^[^@]+@[^@]+\.[^@]+$/, 'Enter a valid email address') .required('*Username is a required field'),
+      username: yup.string().email('Enter a valid email address').required('*Username is a required field'),
       password: yup
             .string()
             .required('*Password is a required field')
