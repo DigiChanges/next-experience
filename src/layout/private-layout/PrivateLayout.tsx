a3af34f46752eb1355622e220d0750788328f913
Alexis Graff <alexisgraff123@gmail.com>
2023-12-10 15:49:02 -0300

    fix: deleting container toastify from layout

--- src/layout/private-layout/PrivateLayout.tsx
+++ src/layout/private-layout/PrivateLayout.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { NavbarTemplate } from '@/features/navbar/template/NavbarTemplate';
 import style from './private-layout.module.css';
-import { Bounce, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 interface Props {
@@ -15,10 +14,6 @@ export const PrivateLayout = (props: Props) => {
   return (
     <>
       <NavbarTemplate/>
-      <ToastContainer
-        autoClose={3000}
-        transition={Bounce}
-        theme={'dark'}/>
       <div className={`${style.containerComponents}`}>
         {children}
       </div>
