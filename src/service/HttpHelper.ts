fdef7bee0739c75e03bb338792b4b27a9aa3ea2e
Alexis Graff <alexisgraff123@gmail.com>
2023-12-15 18:27:00 -0300

    feat: create authorization at getDefaultHeaders

--- src/service/HttpHelper.ts
+++ src/service/HttpHelper.ts
@@ -1,15 +1,24 @@
 import { QueryParams } from './IHttpParams';
 import { config as Config } from '../features/shared/actions/config';
+import { cookies } from 'next/headers';
 
 export function getDefaultHeaders(): Record<string, any> {
   const { credentials } = Config.apiGateway.server;
 
+  const cookieStore = cookies();
+  const cookieValue =  cookieStore.get('sb-xovsaxzresdetrmkouss-auth-token');
+  let token;
+
+  if (cookieValue) {
+    token = JSON.parse(cookieValue.value).access_token;
+  }
+
   return {
     credentials,
     headers: {
-      'Content-Type': 'application/json'
-    },
-    cache: 'no-store'
+      'Content-Type': 'application/json',
+      'Authorization': token ? `Bearer ${token}` : ''
+    }
   };
 }
 
