7d1bbc8695625a5a1facdf1b0cafcf11a94c2ecd
Alexis Graff <alexisgraff123@gmail.com>
2023-12-23 14:56:34 -0300

    fix: add export statement to page.tsx file

--- src/app/items/page.tsx
+++ src/app/items/page.tsx
@@ -2,7 +2,7 @@ import React, { Suspense } from 'react';
 import { ItemsTemplate } from '@/features/items/template/ItemsTemplate';
 import { LoaderStarsWars } from '@/features/shared/atoms/loader/LoaderStarsWars';
 
-
+export const revalidate = 0;
 export default async function Page() {
   return (
 
