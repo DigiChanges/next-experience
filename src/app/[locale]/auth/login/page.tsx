bf16f1b501d98f46b3faa40adf3c5de1835b41a4
Alexis Graff <alexisgraff123@gmail.com>
2023-12-23 20:03:13 -0300

    feat: using t for translations

--- src/app/[locale]/auth/login/page.tsx
+++ src/app/[locale]/auth/login/page.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { LoginTemplate } from '@/features/auth/login/template/LoginTemplate';
 import { PublicLayout } from '@/layout/public-layout/PublicLayout';
 import { unstable_setRequestLocale } from 'next-intl/server';
-import {useTranslations} from 'next-intl';
 import {getTranslations} from "next-intl/server";
+import { useTranslations } from 'next-intl';
 
 type Props = {
     readonly params: {locale: string};
@@ -16,11 +16,11 @@ export default function Page({ params: { locale } }: Props) {
     const t = await getTranslations({locale, namespace: 'LocaleLayout'});
 
   return (
-      <PublicLayout>
-          <div className="max-w-[490px]">
-              <p>{t('title')}</p>
-          </div>
-          <LoginTemplate/>
-      </PublicLayout>
+    <PublicLayout>
+      <div className="max-w-[490px]">
+        <p>{t.raw('title')}</p>
+      </div>
+      <LoginTemplate/>
+    </PublicLayout>
   );
 }
