4205d61f99cac19e53a0e2101b710242668497f0
Alexis Graff <alexisgraff123@gmail.com>
2023-12-24 17:04:00 -0300

    fix: deleting validation of login schema

--- src/features/auth/login/validations/loginSchema.ts
+++ src/features/auth/login/validations/loginSchema.ts
@@ -5,6 +5,6 @@ export const loginSchema = yup
   .object()
   .shape({
     username: yup.string().email('*Email is invalid').required('*Username is a required field'),
-    password: yup.string().min(8, '\'*Min length is 8\'').required('*Password is a required field')
+    password: yup.string().required('*Password is a required field')
   })
   .required();
