931b7a24ddb715bb21fee3b6fbfd1776e8331751
Mariel Torres <marieltorres97@gmail.com>
2024-01-02 13:13:32 -0300

    fix: errors

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -1,10 +1,10 @@
 'use client';
- import React from 'react';
+import React from 'react';
 // TODO: Mejorar la vista
-const error = ({ error , reset }: { error: Error, reset: () => void }) => {
-     return <div className={'text-white'}>
-         <p>There was an error</p>
-        <button onClick={reset} >Try again</button></div>;
- };
+const error = ({ reset }: { error: Error, reset: () => void }) => {
+  return <div className={'text-white'}>
+    <p>There was an error</p>
+    <button onClick={reset} >Try again</button></div>;
+};
 
- export default error;
+export default error;
