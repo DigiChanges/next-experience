b011f57ba688253095da4f170a32bf3a00c555fa
mariel torres <marieltorres97@gmail.com>
2024-01-16 20:57:00 -0300

    fix files storyboook

--- src/features/shared/atoms/swich/swich.tsx
+++ src/features/shared/atoms/swich/swich.tsx
@@ -1,5 +1,5 @@
 'use client';
-import React from 'react'
+import React from 'react';
 import { useEffect, useState } from 'react';
 import { useTheme } from 'next-themes';
 import { Switch } from '@nextui-org/react';
