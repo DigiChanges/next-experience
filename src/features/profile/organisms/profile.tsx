bcdfbf9f6c0d664e93165b1673f4e3c1f0e5c3fd
MelHellrigl <hellriglandreamelissa@gmail.com>
2024-09-24 15:20:27 -0300

    Added info in Profile section

--- src/features/profile/organisms/profile.tsx
+++ src/features/profile/organisms/profile.tsx
@@ -1,9 +1,41 @@
 import React from 'react';
+import { useTranslations } from 'next-intl';
+import style from './profile.module.css';
+import Link from 'next/link';
 
 export const Profile = () => {
+  const t = useTranslations('Profile');
+
   return (
-    <div>
-      <h1>hi, I&#39;m Profile</h1>
+    <div className={style.container}>
+      <div className={style.containerUrl}>
+        <p>{t('home')}</p><span>›</span><Link href={'/dashboard'}>{t('Dashboard')}</Link><span>›</span><p className={style.urlActive}>{t('Profile')}</p>
+      </div>
+      <div className={style.containerProfile}>
+        <h1 data-aos="fade-down" data-aos-duration="1500">
+          {t('title')}
+        </h1>
+        <h2>{t('description')}</h2>
+        <div className={style.containerList}>
+          <div>
+            <p>{t('p_name')}:</p>
+            <p>Maria</p>
+          </div>
+          <div>
+            <p>{t('p_lastname')}:</p>
+            <p>Pepita</p>
+          </div>
+          <div>
+            <p>{t('p_email')}:</p>
+            <p>pepita@gmail.com</p>
+          </div>
+          <div>
+            <p>{t('p_phone')}:</p>
+            <p>123123123</p>
+          </div>
+        </div>
+        <Link href={'/settings'}>Editar contraseña</Link>
+      </div>
     </div>
   );
 };
