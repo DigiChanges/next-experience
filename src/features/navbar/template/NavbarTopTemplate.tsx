2bf4c8b444d344d28d5a7c0670ccee99f5749bab
Mia Passalia <miau.passalia99@hotmail.com>
2024-10-04 14:11:40 -0300

    fix

--- src/features/navbar/template/NavbarTopTemplate.tsx
+++ src/features/navbar/template/NavbarTopTemplate.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { NavbarTop } from '@/features/navbar/organisms/NavbarTop';
-// import { getUser } from '@/features/profile/actions/ProfileAction';
+import { getUser } from '@/features/profile/actions/ProfileAction';
 
 type Props = {
   isPublic: boolean;
 }
 
 export const NavbarTopTemplate = async(props: Props) => {
-  // const user = await getUser();
+  const user = await getUser();
   return (
-    <NavbarTop isPublic={props.isPublic} user={undefined} />
+    <NavbarTop isPublic={props.isPublic} user={user} />
   );
 };
