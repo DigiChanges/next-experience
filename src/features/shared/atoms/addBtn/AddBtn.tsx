1a1f7a323a4afba0d6615de663ab5d3c4ea5f9e5
Melissa Hellrigl <100803744+MelHellrigl@users.noreply.github.com>
2024-10-17 14:39:12 -0300

    feat/amh/create-user (#90)

--- src/features/shared/atoms/addBtn/AddBtn.tsx
+++ src/features/shared/atoms/addBtn/AddBtn.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 import Link from 'next/link';
-import { useTranslations } from 'next-intl';
 
+import style from '@/features/shared/atoms/addBtn/addBtn.module.css';
 import { ButtonForm } from '@/features/shared/atoms/button/ButtonForm';
 import { icons } from '@/features/shared/hooks/icons';
 
-import style from './add-item.module.css';
-
-export const AddItemBtn = () => {
+interface Props {
+  linkButton: string;
+  ariaLabelButton: string;
+  textButton: string;
+}
+export const AddBtn = ({ linkButton, ariaLabelButton, textButton }: Props) => {
   const { IoAddOutline } = icons();
-  const t = useTranslations('Items');
 
   return (
     <div className={style.container}>
-      <Link href={'items/create'}>
-        <ButtonForm ariaLabel='Like'>
+      <Link href={linkButton}>
+        <ButtonForm ariaLabel={ariaLabelButton}>
           <IoAddOutline />
-          {t('addItem')}
+          {textButton}
         </ButtonForm>
       </Link>
     </div>
--- src/features/users/organisms/formCreate/FormCreateUser.tsx
+++ src/features/users/organisms/formCreate/FormCreateUser.tsx
@@ -6,28 +6,31 @@ import { useForm } from 'react-hook-form';
 import { toast } from 'react-toastify';
 
 import { createItem } from '@/features/items/actions/ItemAction';
-import { Item, ItemPayload } from '@/features/items/interfaces/itemsResponse';
-import { modalSchema } from '@/features/items/validations/modalSchema';
+
 import { BtnFormCreateUpdate } from '@/features/shared/atoms/btnFormCreateUpdate/BtnFormCreateUpdate';
 import { InputForm, InputType } from '@/features/shared/atoms/inputForm/InputForm';
 
-import style from './form-create.module.css';
+import { User, UserPayload } from '@/features/users/interfaces/usersResponse';
+import { createUserSchema } from '@/features/users/validations/usersSchema';
+
+import style from './formCreateUser.module.css';
 
 export const FormCreate = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm<Item>({
-    resolver: yupResolver(modalSchema),
+  } = useForm<User>({
+    resolver: yupResolver(createUserSchema),
   });
-  const t = useTranslations('Items');
+  const t = useTranslations('UserList');
   const s = useTranslations('Shared');
   const alert = useTranslations('ToastCreate');
 
   const [isDisabled, setIsDisabled] = useState<boolean>(false);
 
-  const createAction = async (data: ItemPayload, name: string) => {
+  // TODO: falta reemplazar el createItem
+  const createAction = async (data: UserPayload, name: string) => {
     if (!name || name === 'submitForm') {
       await toast.promise(createItem({ data }), {
         error: alert('error'),
@@ -38,14 +41,14 @@ export const FormCreate = () => {
     }
   };
 
-  const onSubmit = async (data: ItemPayload) => {
+  const onSubmit = async (data: UserPayload) => {
     await createAction(data, 'submitForm');
   };
 
   return (
     <form className={style.form} onSubmit={handleSubmit(onSubmit)}>
       <div>
-        <InputForm<Item>
+        <InputForm<User>
           type={'text'}
           name={'name'}
           label={t('name')}
@@ -57,22 +60,56 @@ export const FormCreate = () => {
           classNameError={style.inputError}
           disabled={isDisabled}
         />
-
-        <InputForm<Item>
-          type={'number'}
-          name={'description'}
-          label={t('description')}
+        <InputForm<User>
+          type={'text'}
+          name={'lastName'}
+          label={t('lastName')}
           register={register}
           errors={errors}
-          id={'description'}
+          id={'lastName'}
           className={style.inputBlock}
           input_type={InputType.SIMPLE}
           classNameError={style.inputError}
           disabled={isDisabled}
         />
+        <InputForm<User>
+          type={'number'}
+          name={'phone'}
+          label={t('phone')}
+          register={register}
+          errors={errors}
+          id={'phone'}
+          className={style.inputBlock}
+          input_type={InputType.SIMPLE}
+          classNameError={style.inputError}
+          disabled={isDisabled}
+        />
+        <InputForm<User>
+          type={'email'}
+          name={'email'}
+          label={t('email')}
+          register={register}
+          errors={errors}
+          id={'email'}
+          className={style.inputBlock}
+          input_type={InputType.SIMPLE}
+          classNameError={style.inputError}
+        />
+        <InputForm<User>
+          errors={errors}
+          id={'password'}
+          name={'password'}
+          register={register}
+          type={'password'}
+          label={t('password')}
+          className={style.inputBlock}
+          input_type={InputType.SIMPLE}
+          classNameError={style.inputError}
+          placeholder={t('password')}
+        />
       </div>
       <BtnFormCreateUpdate
-        linkCancel={t('linkItems')}
+        linkCancel={t('linkUsers')}
         textCancel={s('cancel')}
         disabledButton={isDisabled}
         textSubmit={t('add')}
