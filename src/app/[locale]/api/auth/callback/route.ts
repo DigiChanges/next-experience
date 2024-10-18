56d2163814db0bed1a47563422b10c5fc0adb293
Alexis Graff <alexisgraff123@gmail.com>
2024-10-18 09:55:54 -0300

    fix: redirect register email

--- src/app/[locale]/api/auth/callback/route.ts
+++ src/app/[locale]/api/auth/callback/route.ts
@@ -9,7 +9,6 @@ import type { NextRequest } from 'next/server';
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
-  const next = requestUrl.searchParams.get('next') ?? '/';
 
   if (code) {
     const cookieStore = cookies();
@@ -26,13 +25,13 @@ export async function GET(request: NextRequest) {
         },
       },
     });
+
     const { error } = await supabase.auth.exchangeCodeForSession(code);
 
     if (!error) {
-      return NextResponse.redirect(new URL(`/redirect/transition?next=${next}&method=code`, request.url));
+      return NextResponse.redirect(new URL(`/auth/login`, request.url));
     }
   }
 
-  // URL to redirect to after sign in process completes
   return NextResponse.redirect(requestUrl.origin);
 }
